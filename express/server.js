5c1a56f5e5c21c3f0ecfbbff276561743ed66c20
Ali Mustafa <24090078@lums.edu.pk>
2023-03-15 00:17:08 +0500

    Netlify Serverless Example

---
+++ express/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import serverless from "serverless-http";
+
+dotenv.config();
+
+const app = express();
+const router = express.Router();
+
+app.use(router);
+router.use(morgan("dev"));
+
+router.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+router.get("/test", (req, res) => {
+  res.status(200).send("This is a test route.");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port 3000");
+});
+
+const handler = serverless(app);
+
+export default handler;
