d60c487aac70e08c1ec916b4aa99b60f681f73f2
Ali Mustafa <24090078@lums.edu.pk>
2023-03-15 00:35:38 +0500

    Changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
   entry: "./server.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "functions"),
     filename: "bundle.js",
   },
   target: "node",
